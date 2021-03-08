f227c53cb5a959f59b051e8a4a855e0323b52b2d
Khalil E-rramdani <khalil.e-rramdani@ictmbo.nl>
2021-03-08 15:14:49 +0100

    test

---
+++ main.js
@@ -0,0 +1,16 @@
+const a = ("https://backendcities.herokuapp.com/?cuid=hajIUIksk983LLP11112220&start=1&size=30")
+fetch(a)
+
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        for (const d of data) {
+            console.log(d.name);
+
+            const e = document.createElement("div");
+            e.innerHTML = d.name;
+            document.body.appendChild(e);
+        }
+    });
+
