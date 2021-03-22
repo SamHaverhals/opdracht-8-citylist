eb26836de591d9604e0ac81ce0b3cee9727c25f3
Khalil E-rramdani <khalil.e-rramdani@ictmbo.nl>
2021-03-22 11:56:02 +0100

    stratpoint fix

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const p = ;
+const p = 1;
 const startpoint = ((p - 1) * 30) + 1;
-const a = ("https://backendcities.herokuapp.com/?cuid=hajIUIksk983LLP11112220&start=${startpoint}&size=30");
-fetch(a)
-
-    .then(response => response.json())
+const url = `https://backendcities.herokuapp.com/?cuid=hajIUIksk983LLP11112220&start=${startpoint}&size=30`;
+fetch(url)
+    .then(function(response ) { return response.json()})
     .then(data => {
         console.log(data);
 
@@ -15,3 +14,15 @@ fetch(a)
             document.body.appendChild(e);
         }
     });
+
+// const x = {
+//     a: (x)=>console.log(x)
+// };
+// console.log(x.a()); //1
+//
+// const y = [1,2,3];
+// console.log(y.)
+
+
+// const el = document.getElementById('d');
+// el.innerText = 'asdf';
