e9df10af78c703050110b7c9ba7dda381560f76c
Sam Haverhals <sam.haverhals@ictmbo.nl>
2021-03-22 15:29:38 +0100

    ewaa

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 
 const queryString = require('query-string');
-
-
 const parsed = queryString.parse(location.search);
-const p = parsed.page; 
+
+let p =1;
+if (parsed.p){
+    p = parsed.p;
+}
+
+
+
 const startpoint = ((p - 1) * 30) + 1;
+
+
 const url = `https://backendcities.herokuapp.com/?cuid=hajIUIksk983LLP11112220&start=${startpoint}&size=30`;
 
 
@@ -21,3 +28,15 @@ fetch(url)
             document.body.appendChild(e);
         }
     });
+
+
+const back = document.createElement('a');
+back.href = `index.html?p=${(p - 1)}`;
+back.innerText = 'Previous Page' + " ";
+
+const forward = document.createElement('a');
+forward.href = `index.html?p=${(parseInt(p) + 1)}`;
+forward.innerText = 'Next Page';
+
+const d = document.getElementById('states');
+d.append(back,forward);
